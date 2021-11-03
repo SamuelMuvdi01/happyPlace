e59d28a4e72c7444ae45fa852e648571261ef298
builtbydc <dcarl026@fiu.edu>
2021-11-03 16:43:09 -0400

    caching with serviceworker

--- docs/sw.js
+++ docs/sw.js
@@ -1,1 +1,21 @@
-self.addEventListener("fetch", function (event) { });
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(
+                [
+                    "./index.html",
+                    "./style.css",
+                    "./manifest.webmanifest",
+                    "./js/app.js",
+                    "./js/default_list.json",
+                    "./assets/fonts/comfortaa.ttf",
+                    "./assets/fonts/dripicons-v2.woff",
+                    "./assets/icons/apple-touch-icon.png",
+                    "./assets/icons/favicon.ico",
+                    "./assets/icons/hp-icon192.png",
+                    "./assets/icons/hp-icon512.png"
+                ]
+            );
+        })
+    );
+});
